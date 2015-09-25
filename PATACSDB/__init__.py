e8941ffe67e3678edbfc11f240fa5b5523c05fb3
habich <malgorzata.habich@gmail.com>
2015-09-25 02:59:49 +0200

    small changes

--- PATACSDB/__init__.py
+++ PATACSDB/__init__.py
@@ -4,5 +4,9 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///AA.db'
+app.config["APPLICATION_ROOT"] = '/patacsdb'
 
 import PATACSDB.views
+
+if __name__ == "__main__":
+    app.run()
